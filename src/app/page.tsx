ee153f17c2be443c5e346ad918fc9867cb4bf30d
gabrielgcarvalho <gabrielg@objeto.ca>
2023-05-07 17:28:52 -0300

    Change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Navbar from "../lib/components/navbar";
-
 export default function Home() {
     return (
         <>
-            <Navbar />
+            <nav className="bg-white/90 z-10 h-12 w-full top-0 fixed flex flex-row justify-between p-3">
+                <h1 className="basis-1/2 text-lg font-bold bg-gradient-to-r bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 inline-block text-transparent bg-clip-text">
+                    OpenStore
+                </h1>
+                <div className="basis-1/1">
+                    <a className="text-black/80 p-2">Documentaton</a>
+                    <a className="text-black/80 p-2">About</a>
+                    <a className="text-black/80 p-2">Get Started</a>
+                </div>
+            </nav>
             <main className="flex min-h-screen flex-col items-center m-20">
                 <div className="relative flex place-items-center flex-col">
                     <p className="font-bold text-8xl text-center font-sans bg-gradient-to-r bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 inline-block text-transparent bg-clip-text">
